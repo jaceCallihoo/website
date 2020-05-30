7e76903933d0b079d264313c322981ab784ca9fa
Jace Callihoo <jacecallihoo@gmail.com>
2020-05-29 18:09:25 -0600

    removed tailwind and smooth scroll, added to the home page

--- script.js
+++
@@ -1,8 +0,0 @@
-// Smooth Scroll - https://github.com/cferdinandi/smooth-scroll
-const scroll = new SmoothScroll('.navbar a[href*="#"]', {
-    speed: 400
-});
-
-window.addEventListener('scroll', function() {
-    document.getElementById('showScroll').innerHTML = window.pageYOffset + 'px';
-  });
