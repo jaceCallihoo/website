50687923a105b341f9e9309df68a03a19a7d1501
Jace <jcalliho@ualberta.ca>
2021-01-10 12:37:09 -0700

    prep for testing ajax

--- scripts/script.js
+++ scripts/script.js
@@ -77,11 +77,56 @@ function setPage(target) {
 }
 
 // Form stuff ------------------------------------------------------->
+// include("https://smtpjs.com/v3/smtp.js")
+
+var Email = { send: function(a)
+	{
+		return new Promise(function(n, e)
+		{
+			a.nocache = Math.floor(1e6 *Math.random() + 1), a.Action = "Send";
+			var t = JSON.stringify(a);
+			Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function(e)
+			{
+				n(e) }) })
+	}, ajaxPost: function(e, n, t)
+	{
+		var a = Email.createCORSRequest("POST", e);
+		a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function()
+		{
+			var e = a.responseText;
+			null != t && t(e)
+		}, a.send(n)
+	}, ajax: function(e, n)
+	{
+		var t = Email.createCORSRequest("GET", e);
+		t.onload = function()
+		{
+			var e = t.responseText;
+			null != n && n(e)
+		}, t.send()
+	}, createCORSRequest: function(e, n)
+	{
+		var t = new XMLHttpRequest;
+		return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t
+	}
+};
+
 let form = document.getElementById('contact-form');
 let email = document.getElementById('contact-email');
 
 form.addEventListener('submit', (e) => {
+	e.preventDefault();
     console.log("submit");
+
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+	    if (this.readyState == 4 && this.status == 200) {
+	      this.responseText = "a";
+	      document.getElementById("demo").innerHTML = this.responseText;
+	  	}
+	};
+	xhttp.open("POST", "demo_post.asp", true);
+	xhttp.send();
 });
 
 // expand on this function with a better solutino that alerts
